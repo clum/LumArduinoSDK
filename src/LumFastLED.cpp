1cb4d144a1c3b1c0dea8d374edcc830a6dd7177d
Christopher Lum <christopher.w.lum@gmail.com>
2021-12-26 23:01:50 -0800

    Adding sinPulse

--- src/LumFastLED.cpp
+++ src/LumFastLED.cpp
@@ -1,6 +1,12 @@
 #include "LumFastLED.h"
 #include "FastLED.h"
 
+/*
+* Version History
+* 12/??/21: Created
+* 12/26/21: Added allOn and sinPulse
+*/
+
 /// <summary>
 /// Turn off all LEDs
 /// </summary>
@@ -8,9 +14,34 @@
 /// <param name="numLEDs"></param>
 void allOff(CRGB* leds, int numLEDs)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     //turn off all LEDs
     for (int k = 0; k < numLEDs; k++) {
         leds[k] = CRGB(0, 0, 0);        
+    }
+}
+
+/// <summary>
+/// Turn on all LEDs to a specified color
+/// </summary>
+/// <param name="leds"></param>
+/// <param name="numLEDs"></param>
+/// <param name="color"></param>
+void allOn(CRGB* leds, int numLEDs, CRGB color)
+{
+    /*
+    * Version History
+    * 12/26/21: Created
+    */
+
+    //turn on all LEDs
+    for (int k = 0; k < numLEDs; k++) {
+        leds[k] = color;
     }
 }
 
@@ -26,6 +57,12 @@ void allOff(CRGB* leds, int numLEDs)
 /// <param name="deltaT_ms">optional: time between subsequent LEDs (ms)</param>
 void rise(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     static uint16_t sLastMillis = 0;
     static unsigned int numLit = 0;
 
@@ -37,8 +74,7 @@ void rise(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
     if (delta_ms > deltaT_ms) {
         if (numLit == numLEDs - 1) {
             numLit = 0;
-        } 
-        else {
+        } else {
             numLit += 1;
         }
 
@@ -49,8 +85,7 @@ void rise(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
     for (int k = 0; k < numLEDs; k++) {
         if (k <= numLit) {
             leds[k] = color;
-        } 
-        else {
+        } else {
             leds[k] = CRGB(0, 0, 0);
         }
     }
@@ -65,6 +100,12 @@ void rise(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 /// <param name="deltaT_ms">optional: time between subsequent LEDs (ms)</param>
 void fall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     static uint16_t sLastMillis = 0;
     static unsigned int numLit = 0;
 
@@ -76,8 +117,7 @@ void fall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
     if (delta_ms > deltaT_ms) {
         if (numLit == 0) {
             numLit = numLEDs - 1;
-        } 
-        else {
+        } else {
             numLit -= 1;
         }
 
@@ -88,8 +128,7 @@ void fall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
     for (int k = 0; k < numLEDs; k++) {
         if (k <= numLit) {
             leds[k] = color;
-        } 
-        else {
+        } else {
             leds[k] = CRGB(0, 0, 0);
         }
     }
@@ -104,6 +143,12 @@ void fall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 /// <param name="deltaT_ms">optional: time between subsequent LEDs (ms)</param>
 void riseAndFall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     static uint16_t sLastMillis = 0;
     static unsigned int numLit = 0;
     static bool directionRise = 1;
@@ -118,18 +163,15 @@ void riseAndFall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
             //rising
             if (numLit == numLEDs - 1) {
                 directionRise = 0;
-            }
-            else {
+            } else {
                 numLit += 1;
             }
 
-        }
-        else {
+        } else {
             //falling
             if (numLit == 0) {
                 directionRise = 1;
-            }
-            else {
+            } else {
                 numLit -= 1;
             }
 
@@ -142,8 +184,7 @@ void riseAndFall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
     for (int k = 0; k < numLEDs; k++) {
         if (k <= numLit) {
             leds[k] = color;
-        }
-        else {
+        } else {
             leds[k] = CRGB(0, 0, 0);
         }
     }
@@ -159,6 +200,12 @@ void riseAndFall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms)
 /// <returns></returns>
 void rainbowLum(CRGB* leds, int numLEDs)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     static uint16_t sPseudotime = 0;
     static uint16_t sLastMillis = 0;
     static uint16_t sHue16 = 0;
@@ -206,6 +253,12 @@ void rainbowLum(CRGB* leds, int numLEDs)
 /// <param name="chanceOfGlitter">See addGlitter</param>
 void rainbowWithGlitterLum(CRGB* leds, int numLEDs, fract8 chanceOfGlitter)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     rainbowLum(leds, numLEDs);
     addGlitter(leds, numLEDs, chanceOfGlitter);
 }
@@ -219,6 +272,12 @@ void rainbowWithGlitterLum(CRGB* leds, int numLEDs, fract8 chanceOfGlitter)
 /// <param name="deltaT_ms"></param>
 void confettiLum(CRGB* leds, int numLEDs, uint8_t gHue, uint16_t deltaT_ms)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     //function options
     uint8_t fadeBy = 10;
 
@@ -257,12 +316,66 @@ void confettiLum(CRGB* leds, int numLEDs, uint8_t gHue, uint16_t deltaT_ms)
 /// <param name="gHue"></param>
 void bpmLum(CRGB* leds, int numLEDs, uint8_t beatsPerMinute, uint8_t gHue)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
+
     // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
     CRGBPalette16 palette = PartyColors_p;
     uint8_t beat = beatsin8(beatsPerMinute, 64, 255);
     for (int i = 0; i < numLEDs; i++) {
         leds[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
     }
+}
+
+/// <summary>
+/// Vary between two colors in a sinusoidal fashion
+/// </summary>
+/// <param name="leds"></param>
+/// <param name="numLEDs"></param>
+/// <param name="colorA"></param>
+/// <param name="colorB"></param>
+/// <param name="omega_rps">angular frequency of sin wave (rad/s)</param>
+void sinPulse(CRGB* leds, int numLEDs, CRGB colorA, CRGB colorB, float omega_rps)
+{
+    /*
+    * Version History
+    * 12/26/21: Created
+    */
+    uint16_t ms = millis();
+    float t = float(ms) / 1000;
+
+    double y = sin(omega_rps * t);
+
+    //extract RGB components
+    uint8_t rA = colorA.red;
+    uint8_t gA = colorA.green;
+    uint8_t bA = colorA.blue;
+
+    uint8_t rB = colorB.red;
+    uint8_t gB = colorB.green;
+    uint8_t bB = colorB.blue;
+
+    //middle value
+    int rM = (rA + rB) / 2;
+    int gM = (gA + gB) / 2;
+    int bM = (bA + bB) / 2;
+
+    //delta from middle value
+    int deltaR = (rA - rB) / 2;
+    int deltaG = (gA - gB) / 2;
+    int deltaB = (bA - bB) / 2;
+
+    //compute current value
+    double r = (double)rM + y * (double)deltaR;
+    double g = (double)gM + y * (double)deltaG;
+    double b = (double)bM + y * (double)deltaB;
+
+    //set current color
+    CRGB color = CRGB(r, g, b);
+    allOn(leds, numLEDs, color);
 }
 
 /// <summary>
@@ -273,6 +386,11 @@ void bpmLum(CRGB* leds, int numLEDs, uint8_t beatsPerMinute, uint8_t gHue)
 /// <param name="chanceOfGlitter">value between 0 and 100 (default value of 20)</param>
 void addGlitter(CRGB* leds, int numLEDs, fract8 chanceOfGlitter)
 {
+    /*
+    * Version History
+    * 12/??/21: Created
+    * 12/26/21: Updated documentation
+    */
     if (random8() < chanceOfGlitter) {
         leds[random16(numLEDs)] += CRGB::White;
     }
--- src/LumFastLED.h
+++ src/LumFastLED.h
@@ -1,9 +1,16 @@
 #ifndef GUARD_LUMHFASTLED_H
 #define GUARD_LUMHFASTLED_H
 
+/*
+* Version History
+* 12/??/21: Created
+* 12/26/21: Updated documentation
+*/
+
 #include "FastLED.h"
 
 void allOff(CRGB* leds, int numLEDs);
+void allOn(CRGB* leds, int numLEDs, CRGB color);
 void rise(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms = 20);
 void fall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms = 20);
 void riseAndFall(CRGB* leds, int numLEDs, CRGB color, uint16_t deltaT_ms = 20);
@@ -11,6 +18,7 @@ void rainbowLum(CRGB* leds, int numLEDs);
 void rainbowWithGlitterLum(CRGB* leds, int numLEDs, fract8 chanceOfGlitter = 20);
 void confettiLum(CRGB* leds, int numLEDs, uint8_t gHue = 200, uint16_t deltaT_ms = 20);
 void bpmLum(CRGB* leds, int numLEDs, uint8_t beatsPerMinute = 60, uint8_t gHue = 200);
+void sinPulse(CRGB* leds, int numLEDs, CRGB colorA, CRGB colorB, float omega_rps);
 
 void addGlitter(CRGB* leds, int numLEDs, fract8 chanceOfGlitter = 20);
 
