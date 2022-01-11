d35c51686cbd117e7d442cad60fa9e87dc801b2f
Christopher Lum <lum@uw.edu>
2022-01-10 22:25:32 -0800

    Adding LumTinyGPSPlus

--- src/LumTinyGPSPlus.h
+++ src/LumTinyGPSPlus.h
@@ -1,9 +1,10 @@
-#ifndef GUARD_LUMHCSR04_H
-#define GUARD_LUMHCSR04_H
+#ifndef GUARD_LUMTINYGPSPLUS_H
+#define GUARD_LUMTINYGPSPLUS_H
 
 #include <Arduino.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
-int HCSR04GetTimeOfFlight_ms(int PinHCS04Trigger, int PinHCS04Echo, unsigned long timeout = 100000);
-int HCSR04GetDistance_cm(int PinHCS04Trigger, int PinHCS04Echo, unsigned long timeout = 100000);
+void DelayAndEncodeGPSViaSoftwareSerial(unsigned long ms, TinyGPSPlus* gps, SoftwareSerial* ss);
 
 #endif
